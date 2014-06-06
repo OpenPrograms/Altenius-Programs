7f2ca47fcbd3530707bf8eb5e5a9651fca94d225
alakazard12 <jacobjm18@gmail.com>
2014-06-06 10:13:48 -0400

    Changed commas to semicolons

--- programs.lua
+++ programs.lua
@@ -1,12 +1,12 @@
 {
   ["tdit"] = {
     files = {
-      ["master/tdit.lua"] = "/",
+      ["master/tdit.lua"] = "/";
     },
     dependencies = {
     },
-        name = "Tweaked Edit",
-        description = "A tweaked version of the edit.lua programs provided with OpenOS.",
-        authors = "Altenius"
+        name = "Tweaked Edit";
+        description = "A tweaked version of the edit.lua programs provided with OpenOS.";
+        authors = "Altenius";
   }
 }
