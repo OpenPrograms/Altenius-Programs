97c507166990ed25522eeda4f494c444744552ab
alakazard12 <jacobjm18@gmail.com>
2014-06-06 09:58:49 -0400

    Added programs.lua

---
+++ programs.lua
@@ -0,0 +1,12 @@
+{
+  ["tdit"] = {
+    files = {
+      ["master/tedit.lua"] = "/",
+    },
+    dependencies = {
+    },
+        name = "Tweaked Edit",
+        description = "A tweaked version of the edit.lua programs provided with OpenOS.",
+        authors = "Altenius"
+  }
+}
